63ddad19e815a120eec7c59d5cc19cc66c64952e
Jonathan <Jonthan.LT@gmail.com>
2014-12-08 02:34:25 +0100

    Gestion des membres

--- symfony/vendor/composer/autoload_psr4.php
+++ symfony/vendor/composer/autoload_psr4.php
@@ -9,4 +9,5 @@ return array(
     'Symfony\\Bundle\\MonologBundle\\' => array($vendorDir . '/symfony/monolog-bundle'),
     'Symfony\\Bundle\\AsseticBundle\\' => array($vendorDir . '/symfony/assetic-bundle'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
+    'FOS\\UserBundle\\' => array($vendorDir . '/friendsofsymfony/user-bundle'),
 );
