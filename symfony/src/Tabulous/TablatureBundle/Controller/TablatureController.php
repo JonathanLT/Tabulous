ef9cb8cf8f7e94641df3a6ecf6167885e2f20aaf
Jonathan <Jonthan.LT@gmail.com>
2014-12-10 16:26:13 +0100

    Lecture .txt tablature

--- symfony/src/Tabulous/TablatureBundle/Controller/TablatureController.php
+++ symfony/src/Tabulous/TablatureBundle/Controller/TablatureController.php
@@ -17,29 +17,36 @@ class TablatureController extends Controller
       'listTablature' => $listTablature
     ));
 
-  } 
+  }
+
+  protected function getUploadDir()
+  {
+      // on se débarrasse de « __DIR__ » afin de ne pas avoir de problème lorsqu'on affiche
+      // le document/image dans la vue.
+      return 'uploads/documents';
+  }
 
   public function tablatureViewAction($id)
     {
-      $em = $this->getDoctrine()->getManager();
-      $tab = $em->getRepository('TablatureBundle:Tablature')->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $tab = $em->getRepository('TablatureBundle:Tablature')->find($id);
 
         if (!$tab) {
             throw $this->createNotFoundException('Unable to find tablature.');
         }
  
         $tab->setderniereconsultation(new \DateTime());
-    	$em->flush();
-
+    	  $em->flush();
+        $tablatures = file('..\src\Tabulous\TablatureBundle\Resources\tablature\\'.$tab->getadressefichier());
         return $this->render('SiteBundle:Site:view.html.twig', array(
-            'tab' => $tab
+            'tab' => $tab, 'affichage' => $tablatures
         ));
     }
 
    public function artisteViewAction($id)
    {
 
-   $tablatures = $this->getDoctrine()->getManager()->getRepository('TablatureBundle:Tablature')->findByidartiste($id);
+        $tablatures = $this->getDoctrine()->getManager()->getRepository('TablatureBundle:Tablature')->findByidartiste($id);
 
         if (!$tablatures) {
             throw $this->createNotFoundException('Unable to find artiste.');
